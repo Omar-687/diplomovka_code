7e9f9ab50e7fb1361ae776ca2e18df09c1007466
Omar-687 <omaralshafeia@gmail.com>
2024-03-25 21:25:43 +0100

    added RL interface etc

--- main.py
+++ main.py
@@ -5,12 +5,15 @@ import numpy as np
 import itertools
 import torch
 from sac import SAC
+# import tensorboard
 from torch.utils.tensorboard import SummaryWriter
 from replay_memory import ReplayMemory
 
+# gym.make("Pendulum-v0")
+
 parser = argparse.ArgumentParser(description='PyTorch Soft Actor-Critic Args')
-parser.add_argument('--env-name', default="HalfCheetah-v2",
-                    help='Mujoco Gym environment (default: HalfCheetah-v2)')
+parser.add_argument('--env-name', default="Pendulum-v0",
+                    help='Gym environment (default: Pendulum-v0)')
 parser.add_argument('--policy', default="Gaussian",
                     help='Policy Type: Gaussian | Deterministic (default: Gaussian)')
 parser.add_argument('--eval', type=bool, default=True,
@@ -48,12 +51,13 @@ args = parser.parse_args()
 
 # Environment
 # env = NormalizedActions(gym.make(args.env_name))
-env = gym.make(args.env_name)
-env.seed(args.seed)
-env.action_space.seed(args.seed)
-
-torch.manual_seed(args.seed)
-np.random.seed(args.seed)
+print(args.env_name)
+env = gym.make("Pendulum-v1")
+# env.seed(args.seed)
+# env.action_space.seed(args.seed)
+#
+# torch.manual_seed(args.seed)
+# np.random.seed(args.seed)
 
 # Agent
 agent = SAC(env.observation_space.shape[0], env.action_space, args)
@@ -68,12 +72,12 @@ memory = ReplayMemory(args.replay_size, args.seed)
 # Training Loop
 total_numsteps = 0
 updates = 0
-
+env_max_episode_steps = 10
 for i_episode in itertools.count(1):
     episode_reward = 0
     episode_steps = 0
     done = False
-    state = env.reset()
+    state = env.reset()[0]
 
     while not done:
         if args.start_steps > total_numsteps:
@@ -86,7 +90,7 @@ for i_episode in itertools.count(1):
             for i in range(args.updates_per_step):
                 # Update parameters of all the networks
                 critic_1_loss, critic_2_loss, policy_loss, ent_loss, alpha = agent.update_parameters(memory, args.batch_size, updates)
-
+                print(i, critic_1_loss, critic_2_loss)
                 writer.add_scalar('loss/critic_1', critic_1_loss, updates)
                 writer.add_scalar('loss/critic_2', critic_2_loss, updates)
                 writer.add_scalar('loss/policy', policy_loss, updates)
@@ -94,14 +98,14 @@ for i_episode in itertools.count(1):
                 writer.add_scalar('entropy_temprature/alpha', alpha, updates)
                 updates += 1
 
-        next_state, reward, done, _ = env.step(action) # Step
+        next_state, reward, done, truncated, info = env.step(action) # Step
         episode_steps += 1
         total_numsteps += 1
         episode_reward += reward
 
         # Ignore the "done" signal if it comes from hitting the time horizon.
         # (https://github.com/openai/spinningup/blob/master/spinup/algos/sac/sac.py)
-        mask = 1 if episode_steps == env._max_episode_steps else float(not done)
+        mask = 1 if episode_steps == env_max_episode_steps else float(not done)
 
         memory.push(state, action, reward, next_state, mask) # Append transition to memory
 
@@ -117,13 +121,13 @@ for i_episode in itertools.count(1):
         avg_reward = 0.
         episodes = 10
         for _  in range(episodes):
-            state = env.reset()
+            state = env.reset()[0]
             episode_reward = 0
             done = False
             while not done:
                 action = agent.select_action(state, evaluate=True)
 
-                next_state, reward, done, _ = env.step(action)
+                next_state, reward, done, truncated, info = env.step(action)
                 episode_reward += reward
 
 
